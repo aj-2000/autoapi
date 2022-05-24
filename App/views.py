3255d81f98a3441d312ac30ba0230bb2fb2edb06
adi-coder2000 <contact.quicktech@gmail.com>
2022-05-24 20:21:38 +0530

    queryOne bug fixed

--- App/views.py
+++ App/views.py
@@ -28,9 +28,9 @@ def queryOne(request, option):
     if request.method == 'GET':
         autoMPG = pd.read_csv(MPG_DATASET_ABSOLUTE_PATH)
         if option == 1:
-            jsonData = autoMPG[autoMPG['Transmission']=='Automatic'][['Price','Mileage Km/L']].head(1).to_json(orient='columns')
+            jsonData = autoMPG[autoMPG['Transmission']=='Automatic'][['Price','Mileage Km/L']].head(200).to_json(orient='columns')
         elif option == 2:
-            jsonData = autoMPG[autoMPG['Transmission']=='Manual'][['Price','Mileage Km/L']].head(500).to_json(orient='columns')
+            jsonData = autoMPG[autoMPG['Transmission']=='Manual'][['Price','Mileage Km/L']].head(200).to_json(orient='columns')
         else:
             html = "<html><body>Incorrect URL</body></html>"
             return HttpResponse(html)
