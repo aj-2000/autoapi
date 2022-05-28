6ac2785dcfc3811852a4b6df212148d679055ead
adi-coder2000 <contact.quicktech@gmail.com>
2022-05-28 21:26:06 +0530

    requirement.txt updated

--- App/api_views/customer_segments_by_cars_specifications.py
+++ App/api_views/customer_segments_by_cars_specifications.py
@@ -10,10 +10,10 @@ def customer_segments_by_cars_specifications(request, option):
     if request.method == 'GET':
         if(option == 0):
             # get percentages of customers with cars grouped by drive train
-            #  assuming no. of cars as sales data, as no sales data is available
+            #  assuming no. of Models as sales data, as no sales data is available
             #  we can easily use 'sales' columns instead if sales data provided
             df = pd.read_csv(CARS_DATA_ABSOLUTE_PATH)
-            df = df.groupby(['Drivetrain'])['Name'].count()
+            df = df.groupby(['Drivetrain'])['Model'].count()
         elif(option == 1):
             # get percentages of customers with cars grouped by Transmission
             #  assuming no. of cars as sales data, as no sales data is available
